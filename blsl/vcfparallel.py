04535c0a3225ba91a033506902e1d6d25130d1e8
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-06-20 12:30:43 +0200

    improvements to vcfparallel

--- blsl/vcfparallel.py
+++ blsl/vcfparallel.py
@@ -15,6 +15,7 @@ import tempfile
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from pathlib import Path
 import uuid
+import re
 
 
 def parallel_regions(vcf, cores=1, npercore=10):
@@ -29,16 +30,16 @@ def parallel_regions(vcf, cores=1, npercore=10):
             yield f"{cname}:{s:09d}-{e:09d}"
 
 
-def one_chunk(vcf, chunk, temp_prefix, filter="", pipeline="", index=True):
-    cmd=f"bcftools view -r {chunk} {filter} -Ou {vcf}"
+def one_chunk(vcf, chunk, temp_prefix, filterarg="", pipeline="", index=True):
+    ofile = f"{temp_prefix}{chunk}.bcf"
+    cmd=f"bcftools view -r {chunk} {filterarg} -Ou {vcf}"
     if pipeline:
         cmd = f"{cmd} | {pipeline}"
-    ofile = f"{temp_prefix}{chunk}.bcf"
+    if index:
+        cmd = f"{cmd} | bcftools view -Ob0 --write-index -o {ofile}"
     with open(ofile + ".log", "wb") as log:
         with open(ofile, "wb") as ofh:
             subprocess.run(cmd, shell=True, stdout=ofh, stderr=log, check=True)
-        if index:
-            subprocess.run(f"bcftools index -f {ofile}", shell=True, stderr=log, check=True)
     return ofile
 
 
@@ -56,22 +57,29 @@ def chunkwise_pipeline(args):
                 regions.add(f"{chrom}:{start0+1}-{end}")
             regions = list(sorted(regions))
         for region in regions:
-            jobs.append(exc.submit(one_chunk, args.vcf, region, args.temp_prefix, filter=args.filter, pipeline=args.commands, index=not args.merge_with_cat))
+            jobs.append(exc.submit(one_chunk, args.vcf, region, args.temp_prefix, filterarg=args.filter, pipeline=args.commands, index=not args.merge_with_cat))
         for job in tqdm(as_completed(jobs), total=len(jobs), unit="chunk", desc="chunkwise variant pipeline"):
             ofile = job.result()
             filestomerge.append(ofile)
     return filestomerge
 
 
-def merge_one(files, prefix, threads=1):
+def merge_one(files, prefix, threads=1, merge_type="fast"):
     fofn = f"{prefix}fofn.txt"
     output = f"{prefix}output.bcf"
     with open(fofn, "w") as fh:
         for file in sorted(files):
             print(file, file=fh)
-    cmd = f"bcftools concat --file-list {fofn} --rm-dup all --allow-overlaps --threads {threads} -Ob0 -o {output} && bcftools index -f {output}"
-    proc = subprocess.run(cmd, shell=True, check=True)
+    merge = "--allow-overlaps --rm-dup all" if merge_type == "slow" else "--naive"
+    cmd = f"(bcftools concat --file-list {fofn} {merge} --threads {threads} -Ob0 --write-index -o {output}) &>{output.log}"
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+    except subprocess.CalledProcessError:
+        with open(output + ".log") as fh:
+            print(fh.read())
+        raise
     return output
+
 
 def merge_results(args, filestomerge):
     if args.merge_with_cat:
@@ -81,7 +89,7 @@ def merge_results(args, filestomerge):
                     shutil.copyfileobj(fh, ofh)
         return args.output
     if args.threads > 4:
-        print("Grouped merge -> {args.threads} groups")
+        print(f"Grouped merge -> {args.threads} groups")
         groups = [list() for _ in range(args.threads)]
         for i, file in enumerate(filestomerge):
             groups[i%args.threads].append(file)
@@ -89,7 +97,7 @@ def merge_results(args, filestomerge):
         with ProcessPoolExecutor(args.threads) as exc:
             jobs = []
             for i, files in enumerate(groups):
-                jobs.append(exc.submit(merge_one, files, f"{args.temp_prefix}merge_group_{i}_"))
+                jobs.append(exc.submit(merge_one, files, f"{args.temp_prefix}merge_group_{i}_", args.merge_type))
             for job in tqdm(as_completed(jobs), total=len(jobs), unit="group"):
                 ofile = job.result()
                 final_merge.append(ofile)
@@ -100,8 +108,10 @@ def merge_results(args, filestomerge):
     with open(fofn, "w") as fh:
         for file in sorted(final_merge):
             print(file, file=fh)
-    cmd = f"bcftools concat --file-list {fofn} --rm-dup all --allow-overlaps --threads {args.threads} -O{args.outformat} -o {args.output}"
-    proc = subprocess.run(cmd, shell=True, check=True)
+    index = "--write-index" if re.match(r"[zb]", args.outformat) else ""
+    merge = "--allow-overlaps --rm-dup all" if args.merge_type == "slow" else "--naive"
+    cmd = f"bcftools concat --file-list {fofn} {merge} --threads {args.threads} -O{args.outformat} -o {args.output} {index}"
+    subprocess.run(cmd, shell=True, check=True)
     return args.output
 
 
@@ -116,6 +126,8 @@ def main(argv=None):
             help="Output file format (passed directly to bcftools -O, see man bcftools but z=vcf.gz and b=bcf)")
     ap.add_argument("-T", "--temp-prefix", default=None,
             help="Temporary output directory")
+    ap.add_argument("-S", "--slow-merge", default="fast", type=str, action="store_const", const="slow", dest="merge_type",
+            help="Use slow bcftools merging (with --allow-overlaps and --remove-duplicates)")
     ap.add_argument("-f", "--filter", default="", type=str,
             help="bcftools view arguments for variant filtering")
     ap.add_argument("-c", "--commands", default="", type=str,
