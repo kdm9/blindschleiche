14b4dcc051410a6e23a45d7eb70db7c943d56e18
Dr. K.D. Murray <foss@kdmurray.id.au>
2025-09-10 13:11:54 +1000

    tagsane: handle even worse gffs

--- blsl/gfftagsane.py
+++ blsl/gfftagsane.py
@@ -4,20 +4,26 @@ from sys import stdout
 from .gffparse import parseGFF3, write_line
 from tqdm import tqdm
 
+
+
 def main(argv=None):
     """Sanitise a messy gff attribute column to just simple tags """
     ap = argparse.ArgumentParser("gfftagsane")
     ap.add_argument("-o", "--output", type=argparse.FileType("wt"), default=stdout,
             help="Output gff file")
     ap.add_argument("-f", "--fields", default="ID,Name,Parent,locus_tag",
-            help="Attribute tags to keep (case sensitive, give multiple times like -f ID -f tag2 -f tag3).")
+            help="Attribute tags to keep (case sensitive, comma separated).")
+    ap.add_argument("--lax", action="store_true",
+            help="Parse messy GFFs")
     ap.add_argument("input")
 
     args = ap.parse_args(argv)
-    args.fields = args.fields.split(",")
+    args.fields = args.fields.split(",") if args.fields else []
 
-    for record in tqdm(parseGFF3(args.input, return_as=dict)):
-        attrs = {k: record["attributes"][k] for k in args.fields if k in record["attributes"]}
+    for record in tqdm(parseGFF3(args.input, return_as=dict, lax=args.lax)):
+        attrs = record["attributes"]
+        if args.fields:
+            attrs = {k: record["attributes"][k] for k in args.fields if k in record["attributes"]}
         record["attributes"] = attrs
         write_line(record, args.output)
 
